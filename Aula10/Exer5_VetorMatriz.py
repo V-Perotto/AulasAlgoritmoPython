e86df80fc20ebccf553073fb8c7fc2974a1551c7
V-Perotto <vittorio.perotto@outlook.com>
2020-05-11 16:23:39 -0300

    Modificando o Exercício 5, agora completo e funcionando.

--- Aula10/Exer5_VetorMatriz.py
+++ Aula10/Exer5_VetorMatriz.py
@@ -3,27 +3,13 @@ import random
 seed(1)
 
 lista = []
-
-lista.append(random.randint(0, 10000))
-lista.append(random.randint(0, 10000))
-lista.append(random.randint(0, 10000))
-lista.append(random.randint(0, 10000))
-
-i = 4
-j = 0
-aux = 0
-
-#a = input('Digite um numero: ')
-
-for i in range(len(lista)):
-    numero = i.split()
-    junto = ''.join(numero)
-    print(lista[i])
-#if aux = lista[i]:
-#    lista[i] = lista[j]
-#    lista[j] = aux
-
-#else:
-#    print('Não é palíndromo')
-
-# INCORRETO ATÉ O MOMENTO
+cont = 0
+for i in range(1000):
+    lista.append(random.randint(0, 10000))
+for j in lista:
+    menor = min(lista[cont:len(lista)])
+    while menor in lista:
+        lista.remove(menor)
+    lista.insert(0, menor)
+    cont = cont + 1
+print(lista)
